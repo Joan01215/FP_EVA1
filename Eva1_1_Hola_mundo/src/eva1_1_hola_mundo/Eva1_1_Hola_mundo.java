2128a46ddb4a6ee3744ef92a155b27800f7a9602
Joan01215 <joangalaxy07@gmail.com>
2025-08-26 15:34:37 -0700

    Add files via upload

---
+++ Eva1_1_Hola_mundo/src/eva1_1_hola_mundo/Eva1_1_Hola_mundo.java
@@ -0,0 +1,10 @@
+package eva1_1_hola_mundo;
+
+public class Eva1_1_Hola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        System.out.println("FUNDAMENTOS DE PROGRAMCION");
+    }
+    
+}
